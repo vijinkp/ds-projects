a3b2b8aaaa9f3673573954b88f2b6ae789daeb2b
Vijin Kotta Parambath <vparambath@indche-d1625-se.yume.com>
2018-04-30 19:11:21 +0530

    train & test code for Occupancy Detection

--- occupancy-detection/data/occupancy_detection_data.py
+++ occupancy-detection/data/occupancy_detection_data.py
@@ -9,7 +9,7 @@ class OccupancyDetectionDataset(Dataset):
 	'''  
 
 	def __init__(self, input_file, root_folder, predictors, target):
-		columns = predictors
+		columns = list(predictors)
 		columns.append(target)
 		self.data_frame = pd.read_csv('{0}/{1}'.format(root_folder, input_file))[columns]
 		self.target = target
@@ -20,4 +20,4 @@ class OccupancyDetectionDataset(Dataset):
 		return len(self.data_frame)
 
 	def __getitem__(self, idx):
-		return { 'X' : self.data_frame[self.predictors].iloc[idx].values , 'Y' :  self.data_frame[self.target].iloc[idx]}
+		return { 'X' : self.data_frame[self.predictors].iloc[idx].values , 'Y' :  self.data_frame[self.target].iloc[idx].reshape(-1)}
--- occupancy-detection/occupancy_detection.py
+++ occupancy-detection/occupancy_detection.py
@@ -3,33 +3,84 @@ import pandas as pd
 from data import OccupancyDetectionDataset
 from torch.utils.data import DataLoader
 import torch.nn as nn
-import torch.Functional as F
+from torch.autograd import Variable
+import numpy as np
 
 
 class OccupancyDetectionNet(nn.Module):
 
-	def __init__(self):
+	def __init__(self, input_size, hidden_size, output_size):
+		super(OccupancyDetectionNet, self).__init__()
+		self.fc1 = nn.Linear(input_size, hidden_size)
+		self.fc2 = nn.Linear(hidden_size, output_size)
+		self.relu = nn.ReLU()
+		self.out_act = nn.Sigmoid()
 
+		torch.nn.init.xavier_uniform_(self.fc1.weight, gain=np.sqrt(2))
+		torch.nn.init.xavier_uniform_(self.fc2.weight, gain=np.sqrt(2))
 
 	def forward(self, x):
-
+		out = self.fc1(x)
+		out = self.relu(out)
+		out = self.fc2(out)
+		out = self.out_act(out)
+		return out
 
 root_folder = '/home/vparambath/Desktop/iith/AML-Assignment'
 predictors = ["Temperature","Humidity","Light","CO2","HumidityRatio"]
-target = "Occupancy"
+target_col = "Occupancy"
 train_file = 'train_data.txt'
 test_file = 'test_data.txt'
-batch_size = 16
+batch_size = 8
 input_size = len(predictors)
 output_size = 1
-num_epochs = 1
+num_epochs = 6
 hidden_size = 5
 
+train_dataset = OccupancyDetectionDataset(train_file, root_folder, predictors, target_col)
+train_dataloader = DataLoader(train_dataset, batch_size= batch_size, shuffle=True, num_workers=4)
 
-dataset = OccupancyDetectionDataset('train_data.txt', root_folder, predictors, target)
-dataloader = DataLoader(dataset, batch_size= batch_size, shuffle=True, num_workers=4)
+# initialize network
+net = OccupancyDetectionNet(input_size, hidden_size, output_size)
 
-for i, batch in enumerate(dataloader):
-	print(batch['X'])
-	print('**********************')
-	print(batch['Y'])
+# Loss and optimizer
+criterion = nn.BCELoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# Training
+net.train()
+for epoch in range(num_epochs):
+ 	for i, batch in enumerate(train_dataloader):
+ 		data, target = Variable(batch['X'].float()), Variable(batch['Y'].float())
+
+		optimizer.zero_grad()
+		output = net(data)
+		loss = criterion(output, target)
+		loss.backward()
+		optimizer.step()
+
+		if (i+1) % 100 == 0:
+			print ('Epoch [{0}/{1}], Step [{2}/{3}], Loss: {4}'.format(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data.item()))
+
+torch.save(net.state_dict(), 'occupancy_detnet.pkl')
+
+# Testing
+net.eval()
+correct = 0
+total = 0
+test_dataset = OccupancyDetectionDataset(test_file, root_folder, predictors, target_col)
+test_dataloader = DataLoader(test_dataset, batch_size= batch_size, shuffle=False)
+
+actual = []
+predicted = []
+for j, test_batch in enumerate(test_dataloader):
+	data, target = Variable(test_batch['X'].float()), Variable(test_batch['Y'].float())
+	output = net(data)
+
+	actual = actual + list(target.numpy().reshape(-1))
+	predicted = predicted + list(output.data.numpy().reshape(-1))
+
+predicted = np.round(predicted)
+pred_df = pd.DataFrame({'Actual': actual, 'Predict': predicted})
+print(pred_df[pred_df.Actual == pred_df.Predict].shape[0]/ float(len(test_dataset)))
+
