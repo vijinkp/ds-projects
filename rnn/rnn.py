0512e0ab01430a3de9921246d38a89c0f0cb5c3c
Vijin Kotta Parambath <vparambath@indche-d1625-se.yume.com>
2018-05-25 18:40:02 +0530

    character RNN added

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -1,62 +1,89 @@
-# https://github.com/spro/practical-pytorch/blob/master/char-rnn-generation/char-rnn-generation.ipynb
-# https://github.com/mcleonard/pytorch-charRNN
-# https://pytorch.org/tutorials/intermediate/char_rnn_generation_tutorial.html
-# https://www.kaggle.com/mikebaik/simple-rnn-with-pytorch
-# https://github.com/pytorch/examples/tree/master/word_language_model
+# https://github.com/spro/char-rnn.pytorch
 import glob, unicodedata, string
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
-import torch.nn.functional as F
+import numpy as np
+import pickle
 
-class RNN(nn.Module):
-	def __init__(self, input_size, hidden_size, output_size, n_layers=1):
-		super(RNN, self).__init__()
-		self.input_size = input_size
-		self.hidden_size = hidden_size
-		self.output_size = output_size
-		self.n_layers = n_layers
+torch.manual_seed(777)
 
-		self.rnn = nn.RNN(self.input_size, self.hidden_size, num_layers=self.n_layers, nonlinearity='tanh', batch_first=True)
-		self.linear = nn.Linear(self.hidden_size, self.output_size)
-		self.softmax = nn.LogSoftmax(dim=1)
-	
-	def forward(self, input, hidden):
-		input = input.unsqueeze(0)
-		output, hidden = self.rnn(input.view(1,1,-1), hidden)
-		output = self.linear(output.view(1,-1))
-		output = self.softmax(output)
-		return output, hidden
+class CharRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, model="rnn", n_layers=1):
+        super(CharRNN, self).__init__()
+        self.model = model.lower()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
 
-	def init_hidden(self):
-		return Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
+        #self.encoder = nn.Embedding(input_size, hidden_size)
+        if self.model == "rnn":
+            self.rnn = nn.RNN(input_size, hidden_size, n_layers)
+        elif self.model == "lstm":
+            self.rnn = nn.LSTM(hidden_size, hidden_size, n_layers)
+        self.decoder = nn.Linear(hidden_size, output_size)
 
-def make_chars_input(chars, vocab):
-    tensor = torch.zeros(len(chars), 1, len(vocab))
-    for ci in range(len(chars)):
-        char = chars[ci]
-        tensor[ci][0][vocab.find(char)] = 1
-    return Variable(tensor)
+    def forward(self, input, hidden):
+        batch_size = input.size(0)
+        output, hidden = self.rnn(input.view(1, batch_size, -1), hidden)
+        output = self.decoder(output.view(batch_size, -1))
+        return output, hidden
 
-def make_target(chars, vocab):
-    letter_indexes = [vocab.find(chars[li]) for li in range(len(chars))]
-    tensor = torch.LongTensor(letter_indexes)
-    return Variable(tensor)
+    def init_hidden(self, batch_size):
+        if self.model == "lstm":
+            return (Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size)),
+                    Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size)))
+       	return Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size))
 
-# Extended ascii of 256 characters
-vocab = ''.join([chr(char) for char in range(256)])
-root_data_folder = '/home/vparambath/Desktop/iith/ds-projects/data/rnn/test_data'
-sequence_len = 200
-no_epochs = 1
-epochs_evalute = [20, 40, 60, 80, 100]
-hidden_size = 100
-lr = 0.001
+def char_tensor(string, vocab):
+    tensor = np.zeros(len(string))
+    for c in range(len(string)):
+        tensor[c] = vocab.index(string[c])
+    return torch.Tensor(tensor).view(1,-1)
 
+def one_hot(x, num_classes):
+	idx = x.long()
+	idx = idx.view(-1, 1)
+	x_one_hot = torch.zeros(x.size()[0] * x.size()[1], num_classes)
+	x_one_hot.scatter_(1, idx, 1)
+	x_one_hot = x_one_hot.view(x.size()[0], x.size()[1], num_classes)
+	return x_one_hot
 
 # corpus creation
 def unicode_to_ascii(s):
 	return ''.join(
 		c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn' and c in vocab)
+
+def generate(network, vocab, prime_str='A', predict_len=200, temperature=0.8):
+    hidden = network.init_hidden(1)
+    prime_input = Variable(one_hot(char_tensor(prime_str, vocab).unsqueeze(0), len(vocab)))
+    predicted = prime_str
+
+    # Use priming string to "build up" hidden state
+    for p in range(len(prime_str) - 1):
+        _, hidden = network(prime_input[:,p], hidden)
+        
+    inp = prime_input[:,-1]
+    
+    for p in range(predict_len):
+    	output, hidden = network(inp, hidden)
+    	output_dist = output.data.view(-1).div(temperature).exp()
+    	top_i = torch.multinomial(output_dist, 1)[0]
+    	predicted_char = vocab[top_i]
+    	predicted += predicted_char
+    	inp = Variable(one_hot(char_tensor(predicted_char, vocab).unsqueeze(0), len(vocab)))
+    return predicted
+
+# Extended ascii of 256 characters
+vocab = ''.join([chr(char) for char in range(256)])
+root_data_folder = '/home/vparambath/Desktop/iith/ds-projects/data/rnn/test_data'
+sequence_len = 200
+no_epochs = 100
+epochs_evaluate = [20, 40, 60, 80, 100]
+hidden_size = 100
+lr = 0.005
+
 
 text_corpus = []
 for file in glob.glob('{0}/*.txt'.format(root_data_folder)):
@@ -65,20 +92,20 @@ for file in glob.glob('{0}/*.txt'.format(root_data_folder)):
 text_corpus = ' '.join(text_corpus)
 
 # initilaise network
-network = RNN(len(vocab), hidden_size, len(vocab))
+network = CharRNN(len(vocab), hidden_size, len(vocab))
 print(network)
 
 optimizer = torch.optim.Adam(network.parameters(), lr=lr)
-criterion = nn.MSELoss()
+criterion = nn.CrossEntropyLoss()
 
 # train network
 total_iter = int(len(text_corpus)/sequence_len)
-start_iter = 0
 epoch_loss = []
+gen_text = []
 for i in range(no_epochs):
 	iter_loss = []
 	avg_loss = 0
-	#network.zero_grad()
+	start_iter = 0
 	optimizer.zero_grad()
 	while start_iter < len(text_corpus):
 		print('Epoch : {0}/{1} ,iter: {2}/{3} ({4}%)'.format(i+ 1, no_epochs, int(start_iter/sequence_len), 
@@ -87,26 +114,37 @@ for i in range(no_epochs):
 		train_chunk = text_corpus[start_iter : start_iter + sequence_len]
 		target_chunk = text_corpus[start_iter + 1 : start_iter + sequence_len + 1]
 
-		train = make_chars_input(train_chunk, vocab)
-		target = make_chars_input(target_chunk, vocab)
-		
-		hidden = network.init_hidden()
+		if not len(train_chunk) == sequence_len:
+			break
+
+		train = Variable(one_hot(char_tensor(train_chunk, vocab), len(vocab)))
+		target = Variable(char_tensor(target_chunk, vocab).type(torch.LongTensor))
+
+		hidden = network.init_hidden(1)
 		network.zero_grad()
 		loss = 0
-		for c in range(train.size()[0]):
-			output, hidden = network(train[c], hidden)
-			loss += criterion(output,target[c])
+		for c in range(sequence_len):
+			output, hidden = network(train[:,c], hidden)
+			loss += criterion(output.view(1, -1), target[:,c])
 
 		loss.backward()
 		optimizer.step()
-		iter_loss.append(loss.data[0]/sequence_len)
-		avg_loss += loss.data[0]/sequence_len
-		print('Loss: {0}'.format(loss.data[0]/sequence_len))
 
+		iter_loss.append(loss.data.item()/sequence_len)
+		avg_loss += loss.data.item()/sequence_len
+		print('Loss: {0}'.format(loss.data.item()/sequence_len))
 		start_iter = start_iter + sequence_len
-
+		
 	avg_loss = avg_loss / float(total_iter)
 	epoch_loss.append(avg_loss)
 
-	#if i in epochs_evalute:
-		# TO DO : evaluate
+	if i+1 in epochs_evaluate:
+		gen_text.append(generate(network, vocab))
+
+# writing results
+with open('epoch_loss.pkl', 'wb') as fp:
+	fp.dump(epoch_loss, fp)
+
+with open('gen_text.pkl', 'wb') as fp:
+	fp.dump(gen_text, fp)
+
